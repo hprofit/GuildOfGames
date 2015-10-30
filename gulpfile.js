d44dd384651255dad1342d8306bba2ef2cd95f72
holden_profit <holdenprofit@gmail.com>
2015-10-30 11:32:11 -0400

    Initial commit for project start.

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    templateCache = require('gulp-angular-templatecache'),
+    cssmin = require('gulp-cssmin'),
+    Server = require('karma').Server;
+
+var PROJ_ROOT = '',
+    BOWER_COMP_ROOT = PROJ_ROOT + 'bower_components',
+    LESS_ROOT = PROJ_ROOT + 'less',
+    SCRIPTS_ROOT = PROJ_ROOT + 'src',
+    TESTS_ROOT = PROJ_ROOT + 'tests/spec',
+    TEMPLATES_ROOT = PROJ_ROOT + 'views',
+    DIST_ROOT = PROJ_ROOT + 'dist';
+
+var styles = [
+        LESS_ROOT + '/*.less'
+    ],
+    stylesAll = [
+        LESS_ROOT + '/**/*.less'
+    ],
+    scripts = [
+        SCRIPTS_ROOT + '/**/*.js'
+    ],
+    css = [
+        DIST_ROOT + '/css/expanded/core.css',
+        BOWER_COMP_ROOT + '/angular-bootstrap/ui-bootstrap-csp.css'
+    ],
+    libs = [
+        BOWER_COMP_ROOT + '/jquery/dist/jquery.js',
+        BOWER_COMP_ROOT + '/angular/angular.js',
+        BOWER_COMP_ROOT + '/angular-ui-router/release/angular-ui-router.js',
+        BOWER_COMP_ROOT + '/angular-bootstrap/ui-bootstrap.js',
+        BOWER_COMP_ROOT + '/lodash/lodash.js'
+    ],
+    templates = [
+        TEMPLATES_ROOT + '/**/*.html'
+    ],
+    tests = [
+        TESTS_ROOT + '/*.js'
+    ];
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', ['appscripts'], function (done) {
+    Server.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, function () {
+        done();
+    });
+});
+
+gulp.task('styles', function () {
+    return gulp.src(styles)
+        .pipe(less({style: 'expanded'}))
+        .pipe(gulp.dest(DIST_ROOT + '/css/expanded'));
+});
+
+gulp.task('cssmin', ['styles'], function () {
+    gulp.src(css)
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(concat('guildOfGames.min.css'))
+        .pipe(gulp.dest(DIST_ROOT + '/css'));
+});
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src(scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Concatenate & Minify JS - LIBRARIES
+gulp.task('libs', function () {
+    return gulp.src(libs)
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest(DIST_ROOT))
+        .pipe(rename('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(DIST_ROOT));
+});
+
+// Concatenate & Minify JS -- APP
+gulp.task('appscripts', function () {
+    return gulp.src(scripts)
+        .pipe(concat('guildOfGames.js'))
+        .pipe(gulp.dest(DIST_ROOT));
+    //.pipe(rename('guildOfGames.min.js'))
+    //.pipe(uglify())
+    //.pipe(gulp.dest(DIST_ROOT));
+});
+
+gulp.task('templates', function () {
+    return gulp.src(templates)
+        .pipe(templateCache('guildOfGames-templates.js', {
+            module: 'guildOfGames.templates',
+            standalone: true
+        }))
+        .pipe(gulp.dest(DIST_ROOT + '/templates'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function () {
+    gulp.watch(scripts, ['appscripts', 'test']);
+    gulp.watch(stylesAll, ['styles', 'cssmin']);
+    gulp.watch(templates, ['templates']);
+    gulp.watch(tests, ['test']);
+});
+
+// Default Task
+gulp.task('default', ['templates', 'appscripts', 'styles', 'cssmin', 'test']);
+
+gulp.task('build-all', ['templates', 'libs', 'appscripts', 'styles', 'cssmin', 'test']);
---
+++ karma.conf.js
@@ -0,0 +1,94 @@
+module.exports = function (config) {
+    var TEST_ROOT = 'tests/spec/',
+        SCRIPT_ROOT = 'src/',
+        DIST_ROOT = 'dist/',
+        BOWER_COMP_ROOT = 'bower_components/';
+
+    config.set({
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
+
+        // list of files / patterns to load in the browser
+        files: [
+            BOWER_COMP_ROOT + 'jquery/dist/jquery.js',
+            BOWER_COMP_ROOT + 'angular/angular.js',
+            BOWER_COMP_ROOT + 'angular-mocks/angular-mocks.js',
+            BOWER_COMP_ROOT + 'angular-ui-router/release/angular-ui-router.js',
+            BOWER_COMP_ROOT + 'angular-bootstrap/ui-bootstrap.js',
+            BOWER_COMP_ROOT + 'lodash/lodash.js',
+
+            DIST_ROOT + 'templates/guildOfGames-templates.js',
+
+            SCRIPT_ROOT + '**/*.js',
+
+            TEST_ROOT + '*.js'
+        ],
+
+        preprocessors: {
+            '**/directives/**/*.js': 'coverage',
+            '**/controllers/**/*.js': 'coverage',
+            '**/models/**/*.js': 'coverage',
+            '**/services/**/*.js': 'coverage'
+            //'src/app.js': 'coverage'
+            //'exampleSvg.html': ['ng-html2js']
+        },
+
+        ngHtml2JsPreprocessor: {
+            moduleName: 'guildOfGames.templates'
+        },
+
+        coverageReporter: {
+            dir: 'tests/jstest/coverage',
+            reporters: [
+                {
+                    type: 'html'
+                },
+                {
+                    type: 'text'
+                }
+            ]
+        },
+
+        // list of files / patterns to exclude
+        exclude: [],
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress', 'coverage'],
+
+        htmlReporter: {
+            outputDir: 'tests/karma_html',
+            templatePath: 'node_modules/karma-html-reporter/jasmine_template.html'
+        },
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        // web server port
+        port: 8082,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
+};
